356d2bfa8e27c9129c505753b2c8f6c861f8288e
Jeremy Barr <jbarr811@mail.endicott.edu>
2019-12-12 09:08:43 -0500

    DB conversion

--- routes/index.js
+++ routes/index.js
@@ -12,44 +12,13 @@ router.post('/', function(req, res, next) {
 	var personInfo = req.body;
 
 
-	if(!personInfo.email || !personInfo.username || !personInfo.password || !personInfo.passwordConf){
+	if(!personInfo.username || !personInfo.password || !personInfo.passwordConf){
 		res.send();
 	} else {
 		if (personInfo.password == personInfo.passwordConf) {
 
-			User.findOne({email:personInfo.email},function(err,data){
-				if(!data){
-					var c;
-					User.findOne({},function(err,data){
+			User.addUser(personInfo.username, personInfo.password);
 
-						if (data) {
-							c = data.unique_id + 1;
-						}else{
-							c=1;
-						}
-
-						var newPerson = new User({
-							unique_id:c,
-							email:personInfo.email,
-							username: personInfo.username,
-							password: personInfo.password,
-							passwordConf: personInfo.passwordConf
-						});
-
-						newPerson.save(function(err, Person){
-							if(err)
-								console.log(err);
-							else
-								console.log('Success');
-						});
-
-					}).sort({_id: -1}).limit(1);
-					res.send({"Success":"Account registered. You may proceed to login."});
-				}else{
-					res.send({"Success":"E-mail already used."});
-				}
-
-			});
 		}else{
 			res.send({"Success":"Passwords no not match."});
 		}
@@ -62,32 +31,21 @@ router.get('/login', function (req, res, next) {
 
 // LOGIN
 router.post('/login', function (req, res, next) {
-	User.findOne({email:req.body.email},function(err,data){
-		if(data){
-			
-			if(data.password==req.body.password){
-				req.session.userId = data.unique_id;
-				res.send({"Success":"Success!"});
-				
-			}else{
-				res.send({"Success":"Incorrect password!"});
-			}
-		}else{
-			res.send({"Success":"This E-mail has not been regestered!"});
-		}
-	});
+
+	var check = User.checkLogin(req.body.username, req.body.password);
+
+	if(check["isSuccess"]) {
+		req.session.userId = check["userId"];
+		res.send({"Success":"Success!"});
+	} else {
+		res.send({"Success":"Incorrect username/password!"});
+	}
 });
 
 router.get('/profile', function (req, res, next) {
 	console.log("profile");
-	User.findOne({unique_id:req.session.userId},function(err,data){
-		console.log("data");
-		console.log(data);
-		if(!data){
-			res.redirect('/');
-			return res.render('data.ejs', {"name":data.username,"email":data.email});
-		}
-	});
+
+	return res.render('data.ejs', {"name": User.getUsername(session.userId), "email": "oof"});
 });
 
 // 	LOGOUT
