3fd07f4c73eb7046f7127bbbac91319ef10559a3
Jeremy Barr <jebzemy@gmail.com>
2019-12-11 21:17:39 -0500

    updated pass

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', function(req, res){
 var con = mysql.createConnection({
 	host: 		'localhost',
 	user: 		'root',
-	password: 	'Patriots19'
+	password: 	'pass'
 });
 
 con.connect(function(err) {
